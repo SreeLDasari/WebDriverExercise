0020568b28b469fdf0a9b3b396a3358685e11df4
Sree <dsrilu@gmail.com>
2021-02-05 16:12:16 +0530

    checking reset

--- src/test/java/practise/LoginTest.java
+++ src/test/java/practise/LoginTest.java
@@ -51,8 +51,12 @@ public class LoginTest {
 		
 		report = new ExtentReports();
 		report.attachReporter(htmlreport);
+<<<<<<< Updated upstream
 		//test.startTest("Verify the title");
 		//change
+=======
+		
+>>>>>>> Stashed changes
 		report.setSystemInfo("Host Name", "Practise");
 		report.setSystemInfo("Environment", "Production");
 		report.setSystemInfo("User Name", "Sree");
